e4be84c1c2f5695aecea388628579b446756af04
Emperyon <91524645+Emperyon@users.noreply.github.com>
2021-11-07 12:52:37 +0330

    Add files via upload

--- Emperyon_/Scripts.js
+++ Emperyon_/Scripts.js
@@ -15,3 +15,37 @@ window.onclick = function(event) {
     }
 }
 //end of light dropdown
+//start of slide section
+var slideIndex = 0;
+showSlides();
+
+
+function showSlides() {
+    var i;
+    var  slides = document.getElementsByClassName('mySlides');
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = 'block';
+    setTimeout(showSlides, 3000);
+}
+//end of slide bar
+//start of bottom text from big programmers
+var slideIndexx = 0;
+showSlidess();
+
+
+function showSlidess() {
+    var j;
+    var  slidess = document.getElementsByClassName('myTexts');
+    for (j = 0; j < slidess.length; j++) {
+        slidess[j].style.display = 'none';
+    }
+    
+    slideIndexx++;
+    if (slideIndexx > slidess.length) {slideIndexx = 1}
+    slidess[slideIndexx-1].style.display = 'block';
+    setTimeout(showSlidess, 4000);
+}
